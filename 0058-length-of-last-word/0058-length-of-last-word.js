bd1ebaadb752f7a199d8068429088d4a5e670189
Ankithashettyaa <51535386+Ankithashettyaa@users.noreply.github.com>
2023-10-14 19:46:20 +0530

    Time: 44 ms (88.75%), Space: 42.3 MB (18.92%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,11 +3,9 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
+   s=s.trim(" ")
     let i=s.length-1;
     let count =0;
-    while(s[i]=== ' ')
-        {i--;
-        }
     while(s[i]!==' '&&i>=0){
         i--;
         count++;
