2cfd149bf2839092421c75a583cf23930c76e2da
Ankithashettyaa <51535386+Ankithashettyaa@users.noreply.github.com>
2023-11-03 22:51:43 +0530

    Time: 77 ms (91.06%), Space: 54.4 MB (52.16%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,18 +3,20 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    let arr = new Array(nums.length).fill(0);
-    let res = new Array(nums.length).fill(0);
-   arr[nums.length-1]=  nums[nums.length-1]
-    for(let i = nums.length-2;i>=0;i--){
-        arr[i] = nums[i]*arr[i+1]
+    const n = nums.length;
+    const res = new Array(n);
+    
+    let leftProduct = 1;
+    for (let i = 0; i < n; i++) {
+        res[i] = leftProduct;
+        leftProduct *= nums[i];
     }
-    res[0]=arr[1];
-    let right = nums[0]
-    for(let i=1;i<nums.length-1;i++){
-        res[i]= right*arr[i+1]
-        right = right*nums[i]
+    
+    let rightProduct = 1;
+    for (let i = n - 1; i >= 0; i--) {
+        res[i] *= rightProduct;
+        rightProduct *= nums[i];
     }
-    res[nums.length-1]= right;
+    
     return res;
 };
